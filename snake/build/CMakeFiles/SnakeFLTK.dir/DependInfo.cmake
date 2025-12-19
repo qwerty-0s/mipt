6202866e2a9e505de44d380a1099039965e8643f
varlag <varya_lagutina@mail.ru>
2025-12-19 08:40:04 +0000

    last ver

---
+++ snake/build/CMakeFiles/SnakeFLTK.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vboxuser/Desktop/snake/main.cpp" "CMakeFiles/SnakeFLTK.dir/main.cpp.o" "gcc" "CMakeFiles/SnakeFLTK.dir/main.cpp.o.d"
+  "/home/vboxuser/Desktop/snake/snakeBack.cpp" "CMakeFiles/SnakeFLTK.dir/snakeBack.cpp.o" "gcc" "CMakeFiles/SnakeFLTK.dir/snakeBack.cpp.o.d"
+  "/home/vboxuser/Desktop/snake/snakeGraph.cpp" "CMakeFiles/SnakeFLTK.dir/snakeGraph.cpp.o" "gcc" "CMakeFiles/SnakeFLTK.dir/snakeGraph.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ snake/build/CMakeFiles/SnakeFLTK.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SnakeFLTK.dir/main.cpp.o"
+  "CMakeFiles/SnakeFLTK.dir/main.cpp.o.d"
+  "CMakeFiles/SnakeFLTK.dir/snakeBack.cpp.o"
+  "CMakeFiles/SnakeFLTK.dir/snakeBack.cpp.o.d"
+  "CMakeFiles/SnakeFLTK.dir/snakeGraph.cpp.o"
+  "CMakeFiles/SnakeFLTK.dir/snakeGraph.cpp.o.d"
+  "SnakeFLTK"
+  "SnakeFLTK.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SnakeFLTK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
